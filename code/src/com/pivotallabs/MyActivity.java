54001a3e3cfd59929709f801eefedbc6311305f9
Tyler Schultz <pair+tyler@pivotallabs.com>
2010-09-28 17:06:51 -0700

    Add Logo. Make Theme light. Better tests.

--- code/src/com/pivotallabs/MyActivity.java
+++ code/src/com/pivotallabs/MyActivity.java
@@ -3,19 +3,22 @@ package com.pivotallabs;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 public class MyActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.main_layout);
 
         findViewById(R.id.press_me_button_id).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ((TextView) findViewById(R.id.output_textview)).setText("Wacka Wa!");
+                ImageView pivotalLogo = (ImageView) findViewById(R.id.pivotal_logo);
+                pivotalLogo.setImageResource(R.drawable.pivotallabs_logo);
+                pivotalLogo.setVisibility(View.VISIBLE);
             }
         });
     }
--- code/test/com/pivotallabs/MyActivityTest.java
+++ code/test/com/pivotallabs/MyActivityTest.java
@@ -1,35 +1,39 @@
 package com.pivotallabs;
 
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static com.pivotallabs.TestHelper.proxyFor;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 @RunWith(FastAndroidTestRunner.class)
 public class MyActivityTest {
     private TextView pressMeButton;
-    private TextView outputTextView;
+    private ImageView pivotalLogo;
 
     @Before
     public void setUp() throws Exception {
         MyActivity myActivity = new MyActivity();
         myActivity.onCreate(null);
         pressMeButton = (TextView) myActivity.findViewById(R.id.press_me_button_id);
-        outputTextView = (TextView) myActivity.findViewById(R.id.output_textview);
+        pivotalLogo = (ImageView) myActivity.findViewById(R.id.pivotal_logo);
     }
 
     @Test
     public void shouldHaveAButtonThatSaysPressMe() throws Exception {
-        assertThat((String) pressMeButton.getText(), equalTo("Press me!"));
+        assertThat((String) pressMeButton.getText(), equalTo("Tests Rock!"));
     }
 
     @Test
-    public void pressingTheButton_shouldCauseTextToAppear() throws Exception {
-        assertThat((String) outputTextView.getText(), equalTo(""));
+    public void pressingTheButton_shouldImageAppear() throws Exception {
+        assertThat(proxyFor(pivotalLogo).visibility, equalTo(View.GONE));
         pressMeButton.performClick();
-        assertThat((String) outputTextView.getText(), equalTo("Wacka Wa!"));
+        assertThat(proxyFor(pivotalLogo).visibility, equalTo(View.VISIBLE));
+        assertThat(proxyFor(pivotalLogo).resourceId, equalTo(R.drawable.pivotallabs_logo));
     }
 }
