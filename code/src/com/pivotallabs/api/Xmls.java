d5640678e167348b8bb533ee0b40d7b7c95aa0b4
Tyler Schultz <tyler@pivotallabs.com>
2010-10-17 10:42:27 -0700

    fix Xml ant compile error

--- code/src/com/pivotallabs/api/Xmls.java
+++ code/src/com/pivotallabs/api/Xmls.java
@@ -2,6 +2,7 @@ package com.pivotallabs.api;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -23,11 +24,15 @@ public class Xmls {
         return (Element) document.getElementsByTagName(tagName).item(index);
     }
 
-    public static String getTextContentOfChild(Element item, String childTagName) {
-        return item.getElementsByTagName(childTagName).item(0).getTextContent();
+    public static String getTextContentOfChild(Element element, String childTagName) {
+        return getText(element.getElementsByTagName(childTagName));
     }
 
-    public static String getTextContentOfChild(Document item, String childTagName) {
-        return item.getElementsByTagName(childTagName).item(0).getTextContent();
+    public static String getTextContentOfChild(Document document, String childTagName) {
+        return getText(document.getElementsByTagName(childTagName));
+    }
+
+    private static String getText(NodeList nodeList) {
+        return nodeList.item(0).getFirstChild().getNodeValue();
     }
 }
