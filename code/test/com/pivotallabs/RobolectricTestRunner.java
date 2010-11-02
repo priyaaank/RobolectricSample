363b2cc5323c68b9e7e8d46c4569aa2741bc805e
Christian Williams & Phil Goodwin <pair+xian+phil@pivotallabs.com>
2010-11-02 14:26:56 -0700

    Fixed broken build

--- code/test/com/pivotallabs/RobolectricTestRunner.java
+++ code/test/com/pivotallabs/RobolectricTestRunner.java
@@ -2,19 +2,11 @@ package com.pivotallabs;
 
 import android.net.Uri;
 import com.xtremelabs.robolectric.AbstractRobolectricTestRunner;
-import com.xtremelabs.robolectric.Loader;
-import com.xtremelabs.robolectric.ShadowWrangler;
 import org.junit.runners.model.InitializationError;
 
 public class RobolectricTestRunner extends AbstractRobolectricTestRunner {
-    private static final ShadowWrangler SHADOW_WRANGLER = ShadowWrangler.getInstance();
-    private static final Loader LOADER = new Loader(SHADOW_WRANGLER);
-
     public RobolectricTestRunner(Class testClass) throws InitializationError {
-        super(testClass, LOADER);
-        setClassHandler(SHADOW_WRANGLER);
-        setTestHelperClass(TestHelper.class);
-        
-        LOADER.delegateLoadingOf(Uri.class.getName());
+        super(testClass, TestHelper.class);
+        delegateLoadingOf(Uri.class.getName());
     }
  }
