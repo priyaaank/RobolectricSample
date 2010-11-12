b03d2e64430a1aa0aabf6f5b36cce880fc87252c
Gregg Van Hove & Phil Goodwin <pair+gvanhove+phil@pivotallabs.com>
2010-11-12 11:03:17 -0800

    Remove unneeded delegation of Android's Uri class

--- code/test/com/pivotallabs/RobolectricSampleTestRunner.java
+++ code/test/com/pivotallabs/RobolectricSampleTestRunner.java
@@ -1,12 +1,10 @@
 package com.pivotallabs;
 
-import android.net.Uri;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
 import org.junit.runners.model.InitializationError;
 
 public class RobolectricSampleTestRunner extends RobolectricTestRunner {
     public RobolectricSampleTestRunner(Class testClass) throws InitializationError {
         super(testClass);
-        delegateLoadingOf(Uri.class.getName());
     }
  }
