12499dda481918a2f2ff34c455063071df11ce6d
Phil Goodwin & Tyler Schultz <pair+phil+tyler@pivotallabs.com>
2010-12-10 11:50:02 -0800

    moved setModule() to createApplication() so that it will be available when onCreate() is called in RobolectricTestRunner.setupApplicationState()

--- code/test/com/pivotallabs/injected/InjectedTestRunner.java
+++ code/test/com/pivotallabs/injected/InjectedTestRunner.java
@@ -1,5 +1,6 @@
 package com.pivotallabs.injected;
 
+import android.app.Application;
 import com.google.inject.Injector;
 import com.xtremelabs.robolectric.Robolectric;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
@@ -12,12 +13,20 @@ public class InjectedTestRunner extends RobolectricTestRunner {
         super(testClass);
     }
 
+    @Override protected Application createApplication() {
+        InjectedApplication injectedApplication = (InjectedApplication)super.createApplication();
+        injectedApplication.setModule(new RobolectricSampleTestModule());
+        return injectedApplication;
+    }
+
     @Override public void prepareTest(Object test) {
         InjectedApplication injectedApplication = (InjectedApplication) Robolectric.application;
-        injectedApplication.setModule(new RobolectricSampleTestModule());
+
+        //This project's application does not extend GuiceInjectableApplication therefore we need to enter the ContextScope manually.
         Injector injector = injectedApplication.getInjector();
         ContextScope scope = injector.getInstance(ContextScope.class);
         scope.enter(injectedApplication);
+
         injector.injectMembers(test);
     }
 }
